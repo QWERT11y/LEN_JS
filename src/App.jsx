abb69aedc89cdabc322b30c87c9dfc3e540a11c4
QWERT11y <hgdd0375@gmail.com>
2025-02-26 21:57:22 +0500

    das

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomeP from "./HomeP/HomeP";
-import Js from "./Js/js.jsx";
+import Js from "./Js/Js";
 import Html from "./Html/Html";
 import LessonHtml1 from "./Html/LessonHtml1";
 import LessonHtml2 from "./Html/LessonHtml2";
@@ -15,7 +15,7 @@ import LessonJs3 from "./Js/LessonJs3";
 import LessonJs4 from "./Js/LessonJs4";
 import LessonJs5 from "./Js/LessonJs5";
 import LessonJs6 from "./Js/LessonJs6";
-import LessonJS7 from "./Js/LessonJS7";
+import LessonJs7 from "./Js/LessonJS7";  // исправлен регистр!
 import LessonJs8 from "./Js/LessonJs8";
 import LessonJs9 from "./Js/LessonJs9";
 import LessonJs10 from "./Js/LessonJs10";
@@ -23,26 +23,25 @@ import LessonJs10 from "./Js/LessonJs10";
 const App = () => {
   return (
     <Routes>
-      
-        <Route path="/lessonHtml1" element={<LessonHtml1 />} />
-        <Route path="/lessonHtml2" element={<LessonHtml2 />} />
-          <Route path="/lessonHtml3" element={<LessonHtml3 />} />
-          <Route path="/lessonHtml4" element={<LessonHtml4 />} />
-          <Route path="/lessonHtml5" element={<LessonHtml5 />} />
-          <Route path="/lessonHtml6" element={<LessonHtml6 />} />
       <Route path="/" element={<HomeP />} />
       <Route path="/js" element={<Js />} />
       <Route path="/html" element={<Html />} />
-      <Route path="/lessonJs1" element={<LessonJs1/>} />
-     <Route path="/lessonJs2" element={<LessonJs2/>} />
-     <Route path="/lessonJs3" element={<LessonJs3/>} />
-     <Route path="/lessonJs4" element={<LessonJs4/>} />
-     <Route path="/lessonJs5" element={<LessonJs5/>} />
-     <Route path="/lessonJs6" element={<LessonJs6/>} />
-     <Route path="/lessonJs7" element={<LessonJS7/>} />
-     <Route path="/lessonJs8" element={<LessonJs8/>} />
-     <Route path="/lessonJs9" element={<LessonJs9/>} />
-     <Route path="/lessonJs10" element={<LessonJs10/>} />
+      <Route path="/lessonHtml1" element={<LessonHtml1 />} />
+      <Route path="/lessonHtml2" element={<LessonHtml2 />} />
+      <Route path="/lessonHtml3" element={<LessonHtml3 />} />
+      <Route path="/lessonHtml4" element={<LessonHtml4 />} />
+      <Route path="/lessonHtml5" element={<LessonHtml5 />} />
+      <Route path="/lessonHtml6" element={<LessonHtml6 />} />
+      <Route path="/lessonJs1" element={<LessonJs1 />} />
+      <Route path="/lessonJs2" element={<LessonJs2 />} />
+      <Route path="/lessonJs3" element={<LessonJs3 />} />
+      <Route path="/lessonJs4" element={<LessonJs4 />} />
+      <Route path="/lessonJs5" element={<LessonJs5 />} />
+      <Route path="/lessonJs6" element={<LessonJs6 />} />
+      <Route path="/lessonJs7" element={<LessonJs7 />} />
+      <Route path="/lessonJs8" element={<LessonJs8 />} />
+      <Route path="/lessonJs9" element={<LessonJs9 />} />
+      <Route path="/lessonJs10" element={<LessonJs10 />} />
     </Routes>
   );
 };
